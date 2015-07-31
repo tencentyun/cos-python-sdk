494e380ab11ebea6ec4ffd145e0664fba3c37438
wangnan8791 <wangnan8791@gmail.com>
2015-07-31 10:34:46 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'qcloud_cos',
-    version = '1.0.3',
+    version = '1.0.0',
     keywords = ('tencentyun', 'qcloud', 'cos'),
     description = 'python sdk for app.qcloud.com',
     license = 'MIT License',
